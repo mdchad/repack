0b318ee6d673025baf51722f92af4530a641fa2e
mdchad <irsyad.muhd@gmail.com>
2022-12-06 02:16:45 +0800

    add playwright scraping

--- pages/api/get-social.ts
+++ pages/api/get-social.ts
@@ -1,6 +1,18 @@
 const { chromium, webkit, firefox } = require('playwright');
 
-export default function handler(req: any, res: any) {
-  const { pid } = req.query
-  res.end(`Post: ${pid}`)
+export default async function handler(req: any, res: any) {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+
+  await page.goto('https://www.namecheckr.com/');
+  await page.type('input[name=name]', req.query.name);
+  await page.click('button[name=btnSubmit]');
+  const response = await page.waitForResponse((response: any) => response.url().includes('facebook') && response.status() === 200);
+  console.log('RESPONSE ' + (await response.body()));
+  const bodyResponse = await response.body()
+  console.log('RESPONSE1 ' + JSON.parse(bodyResponse));
+
+  await browser.close();
+
+  res.end(`Finish: ${bodyResponse}`)
 }
