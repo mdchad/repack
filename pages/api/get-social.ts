13da9d8105dad28d0e3a5562f1039680bb1dd707
Muhammad Irsyad <irsyad.muhd@gmail.com>
2022-12-06 03:27:36 +0800

    Feature/rpk 18 social media handler (#36)
    * add playwright
    
    * add playwright scraping
    
    * add social media handler

---
+++ pages/api/get-social.ts
@@ -0,0 +1,17 @@
+const { chromium, webkit, firefox } = require('playwright');
+
+export default async function handler(req: any, res: any) {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+
+  await page.goto('https://www.namecheckr.com/');
+  await page.type('input[name=name]', req.query.name);
+  await page.click('button[name=btnSubmit]');
+  const response = await page.waitForResponse((response: any) => response.url().includes('facebook') && response.status() === 200);
+  console.log('RESPONSE ' + (await response.body()));
+  const bodyResponse = await response.body()
+
+  await browser.close();
+
+  res.end(bodyResponse)
+}
